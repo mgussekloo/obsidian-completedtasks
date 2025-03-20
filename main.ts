327834bc2c281b60124c11e21a6df703e7823794
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2025-03-20 18:32:24 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ const checklistLineOrdering = {
 
 let shouldReorderCheckboxes = false;
 
-export default class MyPlugin extends Plugin {
+export default class CompletedTasksPlugin extends Plugin {
 	settings: CompletedTasksSettings;
 
 	async onload() {
@@ -25,20 +25,20 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'sort-completed-tasks',
+			name: 'Sort completed tasks',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.sortCheckboxes();
 			}
 		});
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	if (evt.target.classList.contains("task-list-item-checkbox")) {
-		// 		shouldReorderCheckboxes = true;
-		// 	}
-		// });
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			if (evt.target.classList.contains("task-list-item-checkbox")) {
+				shouldReorderCheckboxes = true;
+			}
+		});
 
 		app.workspace.on("editor-change", (editor: Editor, view: MarkdownView) => {
 			shouldReorderCheckboxes = true;
@@ -47,65 +47,11 @@ export default class MyPlugin extends Plugin {
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => {
 			if (shouldReorderCheckboxes) {
-				const leaf = app.workspace.activeLeaf;
-				if (!leaf || !leaf.view || !leaf.view.editor) {
-				  new Notice("🔴error: no active editor");
-				  return;
-				}
+				shouldReorderCheckboxes = false;
 
-				const editor = leaf.view.editor;
-
-				const cursorAnchor = editor.getCursor("anchor");
-				const cursorHead = editor.getCursor("head");
-
-				const currentText = editor.getValue();
-
-				let blocks = [];
-				let lineCollector = [];
-
-				const lines = currentText.split("\n");
-
-				for (let i = 0; i < lines.length; i++) {
-					const line = lines[i];
-					const nextLine = (i + 1 < lines.length) ? lines[i + 1] : false;
-					const hasCursor = i == cursorHead.line;
-
-					lineCollector.push({
-						line,
-						hasCursor
-					});
-
-					if (nextLine === false || this.lineHasChecklist(line) != this.lineHasChecklist(nextLine)) {
-						blocks.push({
-							lines: lineCollector,
-							hasChecklists: this.lineHasChecklist(line)
-						});
-						lineCollector = [];
-					}
-				}
-
-				const sortedLines = blocks
-				.map(block => {
-					let lines = block.lines;
-
-					if (block.hasChecklists) {
-						lines = lines.sort((a, b) => this.lineSortValue(a.line) - this.lineSortValue(b.line));
-					}
-
-					return lines;
-				})
-				.flat();
-
-				const cursorIsAtLine = sortedLines.findIndex(line => line.hasCursor);
-				const newText = sortedLines.map(line => line.line).join("\n");
-
-				if (newText != currentText) {
-					editor.setValue(newText);
-					editor.setSelection({ line: cursorIsAtLine, ch: cursorHead.ch });
-				}
+				this.sortCheckboxes();
 			}
 
-			shouldReorderCheckboxes = false;
 		},  10 * 1000));
 	}
 
@@ -126,12 +72,129 @@ export default class MyPlugin extends Plugin {
 		return Object.keys(checklistLineOrdering)
 		.filter(key => line.startsWith(key))
 		.length;
-		// return (line.startsWith('- [ ]') || line.startsWith('- [x]'));
 	}
 
 	sortCheckboxes() {
+		const leaf = app.workspace.activeLeaf;
+		if (!leaf || !leaf.view || !leaf.view.editor) {
+		  new Notice("🔴 error: no active editor");
+		  return;
+		}
 
+		const editor = leaf.view.editor;
+
+		const cursorAnchor = editor.getCursor("anchor");
+		const cursorHead = editor.getCursor("head");
+
+		const currentText = editor.getValue();
+
+		let blocks = [];
+		let lineCollector = [];
+		let sublineCollector = [];
+
+		const lines = currentText.split("\n");
+		let lastRootChecklistIndex = -1;
+
+		for (let i = 0; i < lines.length; i++) {
+			const line = lines[i];
+			const prevLine = (i - 1 >= 0) ? lines[i - 1] : false;
+			const nextLine = (i + 1 < lines.length) ? lines[i + 1] : false;
+
+			const hasCursor = i == cursorHead.line;
+
+			const currentLineHasRootOrSubChecklist = this.lineHasChecklist(line.trim());
+			const nextLineHasRootOrSubChecklist = nextLine ? this.lineHasChecklist(nextLine.trim()) : nextLine;
+			const currentLineHasRootChecklist = this.lineHasChecklist(line);
+
+			// if this line has NO root checklist, but does have a checklist somewhere,
+			// we've spotted a sub-item
+			if (!currentLineHasRootChecklist && currentLineHasRootOrSubChecklist) {
+				if (lastRootChecklistIndex >= 0) {
+					lineCollector[lastRootChecklistIndex].sublines.push({
+						line,
+						hasCursor
+					});
+				}
+			// else this is a normal line, either a root checklist or a non-checklist line
+			} else {
+				lineCollector.push({
+					line,
+					sublines: [],
+					hasCursor
+				});
+
+				// remember if we passed a root checklist, so we can
+				// add sublines when we encounter them later
+				if (currentLineHasRootChecklist) {
+					lastRootChecklistIndex = lineCollector.length - 1;
+				}
+			}
+
+			// if we encounter the end of the file,
+			// or if we encounter a "block change": either a block of (sub)checkboxes or a block of non (sub)checkboxes
+			// then we save the previous lines as a new block and start again
+			if (
+				nextLine === false
+				|| (currentLineHasRootOrSubChecklist != nextLineHasRootOrSubChecklist)
+			) {
+				blocks.push({
+					lines: lineCollector,
+					hasChecklists: currentLineHasRootChecklist
+				});
+
+				lineCollector = [];
+				lastRootChecklistIndex = -1;
+			}
+		}
+
+		// if there are no checklists anywhere, we bail
+		if (blocks.filter(block => block.hasChecklists).length == 0) {
+			return;
+		}
+
+		// sorting the rootlines
+		const sortedLines = blocks
+		.map(block => {
+			let lines = block.lines;
+
+			if (block.hasChecklists) {
+				lines = lines.sort((a, b) => this.lineSortValue(a.line) - this.lineSortValue(b.line));
+			}
+
+			return lines;
+		})
+		.flat();
+
+		// now make a new array of root lines and their sublines
+		let cursorIsAtLine = cursorHead.line;
+		let newLines = [];
+
+		sortedLines.forEach((line, index) => {
+			cursorIsAtLine = line.hasCursor ? index : cursorIsAtLine;
+
+			newLines.push(line.line);
+			line.sublines.forEach((subline, subindex) => {
+				cursorIsAtLine = subline.hasCursor ? index + subindex : cursorIsAtLine;
+				newLines.push(subline.line)
+			});
+		})
+
+		const newText = newLines.join("\n");
+
+		// if something happened...
+		if (newText != currentText) {
+			editor.setValue(newText);
+			editor.setSelection({ line: cursorIsAtLine, ch: cursorHead.ch });
+		}
 	}
+
+
+
+
+
+
+
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -145,9 +208,9 @@ export default class MyPlugin extends Plugin {
 
 
 class CompletedTasksSettingsTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: CompletedTasksPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CompletedTasksPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
