886c80a5b13ec1eea49363e49bc11dfb7f5bf907
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2025-12-10 21:11:05 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -380,7 +380,7 @@ class CompletedTasksSettingsTab extends PluginSettingTab {
 		.addTextArea(text => text
 			.setValue(this.plugin.settings.sortedStatuses.join(','))
 			.onChange(async (value) => {
-				this.plugin.settings.sortedStatuses = value.split(',').map(item => item && item.trim());
+				this.plugin.settings.sortedStatuses = value.split(',');
 				await this.plugin.saveSettings();
 			}));
 
@@ -390,7 +390,7 @@ class CompletedTasksSettingsTab extends PluginSettingTab {
 			.addTextArea(text => text
 				.setValue(this.plugin.settings.sortedSubstrings.join(','))
 				.onChange(async (value) => {
-					this.plugin.settings.sortedSubstrings = value.split(',').map(item => item && item.trim());
+					this.plugin.settings.sortedSubstrings = value.split(',');
 					await this.plugin.saveSettings();
 				}));
 
